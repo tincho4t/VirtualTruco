b25ea6f6a42636dad33b42582e9e5625c6b8ddc7
Martin Forte <martinforte@yahoo.com.ar>
2018-10-27 16:13:05 -0300

    Saco codigo comentado

--- performance_index.js
+++ performance_index.js
@@ -1,12 +1,6 @@
 var playerBuilders = [
 	function(){ return new apiPlayer("Api Player 1", "8000", false, true)},
-	//function(){ return new HumanPlayer("Human 1")},
 	function(){ return new apiPlayer("Api Player 2", "8001", false, true)},
-	//function(){ return new apiPlayer("Api Player II", "8124", true)},
-	// function(){ return new HumanPlayer("Human 1")},
-	// function(){ return new apiPlayer("Api Player", "8125", false)},
-	//function(){ return new RandomPlayer("Randomio 1")},
-	//function(){ return new RandomPlayer("Randomio 2")},
 ]
 
 
